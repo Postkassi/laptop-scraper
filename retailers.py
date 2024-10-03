af755f45c177cfc9f6bbe49a06713d771a86e53c
Postkassi <birkiremilsson@gmail.com>
2024-10-03 11:57:13 +0000

    Toggled commented Lines

--- retailers.py
+++ retailers.py
@@ -1,56 +1,56 @@
 retailer_urls = {
-    # 'advania': { 
-    #     'baseurl': 'https://vefverslun.advania.is',
-    #     'urls': ('https://vefverslun.advania.is/voruflokkar/fartolvur?ProductID=&feed=true&PageNum=1&PageSize=',)
-    # },
-    #  'computer': { 
-    #      'baseurl': 'https://computer.is',
-    #      'urls': (
-    #          'https://computer.is/is/products/10-14-tommur',
-    #          'https://computer.is/is/products/15-6-tommur',
-    #          'https://computer.is/is/products/17-3-tommur',
-    #      )
-    # },
+    'advania': { 
+        'baseurl': 'https://vefverslun.advania.is',
+        'urls': ('https://vefverslun.advania.is/voruflokkar/fartolvur?ProductID=&feed=true&PageNum=1&PageSize=',)
+    },
+     'computer': { 
+         'baseurl': 'https://computer.is',
+         'urls': (
+             'https://computer.is/is/products/10-14-tommur',
+             'https://computer.is/is/products/15-6-tommur',
+             'https://computer.is/is/products/17-3-tommur',
+         )
+    },
     'elko': { 
         'baseurl': 'https://elko.is',
         'urls': ('https://elko.is/voruflokkar/fartolvur-94',)
     },
-#     'kisildalur': {
-#         'baseurl': 'https://kisildalur.is',
-#         'urls': (28,)
-#     },
-#     # 'macland': { 
-#     #     'baseurl': 'https://macland.is',
-#     #     'urls': (
-#     #         'https://macland.is/flokkur/mac/macbook-air/',
-#     #         'https://macland.is/flokkur/mac/macbook-pro/macbook-pro-13-m1/',
-#     #         'https://macland.is/flokkur/mac/macbook-pro/macbook-pro-16/',
-#     #     )
-#     #},
-#     'origo': {
-#         'baseurl': 'https://verslun.origo.is/api/FetchProducts',
-#         'urls': (847,)
-#         #'urls': (
-#         #    'https://verslun.origo.is/tolvur-og-skjair/fartolvur?productsPerPage=150,'
-#         #    )
-#     },
-#     'opinkerfi': {
-#         'baseurl': 'https://vefverslun.ok.is',
-#         'urls': ('https://vefverslun.ok.is/tolvubunadur/fartolvur',)
-#     },
-#     'tolvulistinn': {
-#         'baseurl': 'https://tl.is',
-#         'urls': ('https://tl.is/fartolvur/allar-staer-ir.html',),
-#     },
-#     'tolvutek': {
-#         'baseurl': 'https://tolvutek.is',
-#         'urls': (847,)
-#     },
-#     'epli': { 
-#         'baseurl': 'https://epli.is',
-#         'urls': (
-#             'https://www.epli.is/computer/macbook-air/',
-#             'https://www.epli.is/computer/macbook-pro/',
-#         )
-#     },
+    'kisildalur': {
+        'baseurl': 'https://kisildalur.is',
+        'urls': (28,)
+    },
+    # 'macland': { 
+    #     'baseurl': 'https://macland.is',
+    #     'urls': (
+    #         'https://macland.is/flokkur/mac/macbook-air/',
+    #         'https://macland.is/flokkur/mac/macbook-pro/macbook-pro-13-m1/',
+    #         'https://macland.is/flokkur/mac/macbook-pro/macbook-pro-16/',
+    #     )
+    #},
+    'origo': {
+        'baseurl': 'https://verslun.origo.is/api/FetchProducts',
+        'urls': (847,)
+        #'urls': (
+        #    'https://verslun.origo.is/tolvur-og-skjair/fartolvur?productsPerPage=150,'
+        #    )
+    },
+    'opinkerfi': {
+        'baseurl': 'https://vefverslun.ok.is',
+        'urls': ('https://vefverslun.ok.is/tolvubunadur/fartolvur',)
+    },
+    'tolvulistinn': {
+        'baseurl': 'https://tl.is',
+        'urls': ('https://tl.is/fartolvur/allar-staer-ir.html',),
+    },
+    'tolvutek': {
+        'baseurl': 'https://tolvutek.is',
+        'urls': (847,)
+    },
+    'epli': { 
+        'baseurl': 'https://epli.is',
+        'urls': (
+            'https://www.epli.is/computer/macbook-air/',
+            'https://www.epli.is/computer/macbook-pro/',
+        )
+    },
  }
